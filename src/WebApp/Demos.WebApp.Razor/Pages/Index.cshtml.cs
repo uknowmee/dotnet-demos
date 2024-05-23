b1e932a7a6200c3dabca3f5caa2fea934503ebfe
Uknowme <michalpalucki@icloud.com>
2024-05-23 22:23:11 +0200

    Add sln

---
+++ src/WebApp/Demos.WebApp.Razor/Pages/Index.cshtml.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Demos.WebApp.Razor.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly ILogger<IndexModel> _logger;
+
+    public IndexModel(ILogger<IndexModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
