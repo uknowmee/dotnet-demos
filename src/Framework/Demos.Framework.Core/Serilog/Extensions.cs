e26fc7d9f2c03ba3378f9305b50ef1223be0db83
Uknowme <michalpalucki@icloud.com>
2024-05-24 03:05:20 +0200

    [2-add-database-to-web-app] Add framework

---
+++ src/Framework/Demos.Framework.Core/Serilog/Extensions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using Serilog.Extensions.Logging;
+
+namespace Demos.Framework.Core.Serilog;
+
+internal static class Extensions
+{
+    public static WebApplicationBuilder AddLogging(this WebApplicationBuilder builder)
+    {
+        builder.Host.UseSerilog();
+        return builder;
+    }
+
+    public static IServiceCollection AddLogger(this IServiceCollection services)
+    {
+        var configuration = new ConfigurationBuilder()
+            .AddJsonFile("appsettings.json", false, true)
+            .Build();
+
+        Log.Logger = new LoggerConfiguration()
+            .ReadFrom.Configuration(configuration)
+            .CreateLogger();
+
+        var factory = new LoggerFactory(new ILoggerProvider[]
+            {
+                new SerilogLoggerProvider(Log.Logger)
+            }
+        );
+
+        services.AddSingleton(factory);
+        return services;
+    }
+
+    public static IApplicationBuilder UseContextLogger(this IApplicationBuilder app) => app.UseSerilogRequestLogging();
+}
