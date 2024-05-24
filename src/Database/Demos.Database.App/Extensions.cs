d825948c3c4d858d8345564ac409374aed310cf6
Uknowme <michalpalucki@icloud.com>
2024-05-24 03:06:58 +0200

    [2-add-database-to-web-app] Add framework to Web App

---
+++ src/Database/Demos.Database.App/Extensions.cs
@@ -0,0 +1,13 @@
+﻿using Demos.Database.App.Weather;
+
+namespace Demos.Database.App;
+
+public static class Extensions
+{
+    public static WebApplicationBuilder AddCore(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddScoped<IWeatherService, WeatherService>();
+        
+        return builder;
+    }
+}
